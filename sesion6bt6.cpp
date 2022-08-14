d107082d9059cbb8bd26e18e37b148d6be95744f
thaoptt26 <109666677+thaoptt26@users.noreply.github.com>
2022-08-14 14:32:02 +0700

    Create sesion6bt6.cpp

---
+++ sesion6bt6.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	int n;
+	printf("nhap kich thuoc mang n:");
+	scanf("%d",&n);
+	int arr[n];
+	printf("nhap mang:\n");
+	for (int i=0;i<n;i++){
+	  scanf("%d",&arr[i]);
+}
+int tm=0;
+int kl=0; 
+for(int i=0;i<n;i++){
+	if(arr[i]>0){
+		tm++;
+		if(tm>kl){
+			kl=tm; 
+		} 
+	} else{
+		tm=0;
+	}
+} 
+printf("so luong so duong lien tiep nhieu nhat:%d",kl);
+}
+

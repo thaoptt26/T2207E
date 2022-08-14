446dc8cf99639e71b10177b721070232176bb3aa
thaoptt26 <109666677+thaoptt26@users.noreply.github.com>
2022-08-14 14:33:11 +0700

    Create sesionbt3.cpp

---
+++ sesionbt3.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int x;
+	printf("nhap  x can tim:\n");
+	scanf("%d",&x);
+	bool t= false;
+	for(int i=0;i<n;i++){
+		if(x==arr[i]){
+			t= true;
+			break; 
+		} 
+	} 
+}
+	if(t){
+		printf("da tim thay gia tri x");
+	}else{
+		printf("khong tim thay"); 
+	} 
+	} 

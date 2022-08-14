95d147c4d6847dd10bdcdb6a9bd8dbb20e87894d
thaoptt26 <109666677+thaoptt26@users.noreply.github.com>
2022-08-14 14:04:18 +0700

    Create bai2.cpp

---
+++ bai2.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("Nhap a=");
+	scanf("%d" ,&a);
+	 
+	int b;
+	printf("Nhap b=");
+	scanf("%d" ,&b);
+	
+	int c;
+	printf("Nhap c=");
+	scanf("%d" ,&c);
+	
+	if(a>b){ if(a>c){
+		printf("Max: %d" ,a);
+	}	else {printf("max:%d",c);
+		}
+	}else {if(b>c){
+		printf("max:%d" ,b);
+}else{printf("max:%d" ,c);
+	}
+	}
+	}
+
